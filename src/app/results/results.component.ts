e98ca991ab370d9279da0268797ab82ca0360354
Cameron Green <i@camerongreen.org>
2020-03-24 17:00:59 +1000

    We are in the infancy of frameworks, angular is still teething

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,9 +1,34 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 import {SearchDetailsService} from '../search-details.service';
 import {DataSourceService} from '../data-source.service';
 import {Subscription} from 'rxjs';
 import {Resource} from '../resource.model';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+
+const ELEMENT_DATA: Resource[] = [
+  {
+    region: 'qld',
+    categories: ['business'],
+    type: 'THing',
+    title: 'I am title',
+    description: 'I am description',
+    url: 'http://www.gogole.com',
+    startDate: '5/12/2020',
+    endDate: '6/12/2020',
+  },
+  {
+    region: 'qld',
+    categories: ['business'],
+    type: 'A THing',
+    title: 'A I am title',
+    description: 'I am description',
+    url: 'http://www.gogole.com',
+    startDate: '5/12/2020',
+    endDate: '4/12/2020',
+  }
+];
 
 @Component({
   selector: 'app-results',
@@ -17,31 +42,36 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     ]),
   ],
 })
-export class ResultsComponent implements OnDestroy {
+export class ResultsComponent implements OnDestroy, OnInit {
   subscription: Subscription;
-  resources: Resource[];
-  columnsToDisplay = {
-    title: 'Title',
-    type: 'Type',
-    endDate: 'End date',
-    more: ''
-  };
-  columnIds = Object.keys(this.columnsToDisplay);
+  resources = new MatTableDataSource<Resource>();
+  columnsToDisplay = [
+    'title',
+    'type',
+    'endDate',
+    'more'
+  ];
   expandedElement: Resource | null;
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(
     private searchDetailsService: SearchDetailsService,
     private dataSourceService: DataSourceService
   ) {
-    this.subscription = searchDetailsService.searchRequested$.subscribe(
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.searchDetailsService.searchRequested$.subscribe(
       query => {
         this.updateQuery(query);
       }
     );
+    this.resources.sort = this.sort;
   }
 
   updateQuery(query): void {
-    this.resources = this.dataSourceService.search(query.state, query.category);
+    this.resources.data = this.dataSourceService.search(query.state, query.category);
   }
 
   ngOnDestroy() {
